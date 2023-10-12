3cdb8fa1745b0798c486d2eb4b6040cb3fd5d8a0
MuelNova <n@ova.moe>
2023-10-12 17:40:43 +0800

    :tada: First commit

---
+++ src/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func generateHMACSignature(secret string, payload []byte) string {
+	h := hmac.New(sha1.New, []byte(secret))
+	h.Write(payload)
+	return "sha1=" + hex.EncodeToString(h.Sum(nil))
+}
+
+func runCommand(command string) error {
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+
+	secret := os.Getenv("GITHUB_SECRET")
+	if secret == "" {
+		log.Fatal("GITHUB_SECRET is not set")
+		os.Exit(1)
+	}
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.POST("/", func(c *gin.Context) {
+		payload, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			log.Fatalf("Error reading body: %v", err)
+			c.AbortWithStatus(400)
+			return
+		}
+
+		if generateHMACSignature(secret, payload) != c.Request.Header.Get("X-Hub-Signature") {
+			log.Fatalf("Invalid HMAC signature: %s", c.GetHeader("X-Hub-Signature"))
+			c.AbortWithStatus(401)
+			return
+		}
+
+		go func() {
+			extraCommand := os.Getenv("EXTRA_COMMAND")
+			err := runCommand(extraCommand) // TODO: push this notice.
+			if err != nil {
+				log.Fatalf("Error running command: %v", err)
+				return
+			}
+		}()
+
+		c.Status(200)
+	})
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "4567"
+	}
+	r.Run(":" + PORT)
+}
